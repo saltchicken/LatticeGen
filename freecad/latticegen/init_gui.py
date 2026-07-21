c9fdd50b8881f7fc2929e211fe4928795a4b16ef
saltchicken <john.eicher89@gmail.com>
2026-07-21 16:22:46 -0700

    Make it a toolbar rather than a workbench

--- freecad/latticegen/init_gui.py
+++ freecad/latticegen/init_gui.py
@@ -1,13 +1,27 @@
-# freecad/freecad.latticegen/init_gui.py
+import FreeCAD as App
+import FreeCADGui as Gui
+
 from .resources import Resources
 from .commands import LatticeGenCommand
-from .lattice_workbench import LatticeGenWorkbench
 
 # 1. Register icons so FreeCAD can find them
 Resources.gui_register_icons()
 
-# 2. Register your commands
+# 2. Register your command
 LatticeGenCommand.Install()
 
-# 3. Register your workbench
-LatticeGenWorkbench.Install()
+# 3. Create a safe Workbench Manipulator
+class PartDesignManipulator:
+    def modifyToolBars(self):
+        return [{"append": LatticeGenCommand.Name, "toolBar": "LatticeGen"}]
+
+    def modifyMenuBar(self):
+        return []
+
+    def modifyContextMenu(self, recipient):
+        return []
+
+if App.GuiUp:
+    # Safely inject the manipulator
+    manipulator = PartDesignManipulator()
+    Gui.addWorkbenchManipulator(manipulator)
